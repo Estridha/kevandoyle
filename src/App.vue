a95afdd675f181d8eef96293478908282fed75ab
estridha <estridhassbring@gmail.com>
2022-11-15 21:53:51 +0100

    added contact view, changed nav menu styling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -16,15 +18,17 @@
 }
 
 nav {
+  text-transform: uppercase;
   padding: 30px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  color: black;
+  margin: 0 1rem;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  font-weight: bold;
 }
 </style>
