19d1f12a15de56d13595ddd7bfa2a4ad9a9afbe2
estridha <estridhassbring@gmail.com>
2022-11-15 23:49:01 +0100

    added sass

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <nav>
+    <p>Kevan Doyle</p>
+    <div class="navigation-menu">
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-link to="/contact">Contact</router-link>
+  </div>
   </nav>
   <router-view/>
 </template>
 
-<style>
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 #app {
+ /*  background-image: url(@/assets/images/background.jpg);
+  background-size: cover; */
+  min-height: 100vh;
   font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -18,6 +24,8 @@
 }
 
 nav {
+  display: flex;
+  justify-content: space-between;
   text-transform: uppercase;
   padding: 30px;
 }
@@ -30,5 +38,13 @@ nav a {
 
 nav a.router-link-exact-active {
   font-weight: bold;
+}
+
+.navigation-menu {
+  display: flex;
+  align-items: center;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 }
 </style>
