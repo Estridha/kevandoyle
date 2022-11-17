389155a3eab2bac32091dc7754105825db7b7475
estridha <estridhassbring@gmail.com>
2022-11-17 02:20:54 +0100

    made nav-menu responsive, added about section

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
+<script>
+export default {
+  data() {
+    return {
+      menuIsOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu: function (event) {
+      this.menuIsOpen = !this.menuIsOpen;
+    },
+  },
+};
+</script>
+
 <template>
   <nav>
     <p>Kevan Doyle</p>
     <div class="navigation-menu">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/contact">Contact</router-link>
-  </div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+    <button class="mobile-menu" v-on:click="toggleMenu">
+      <img v-if="!menuIsOpen" src="@/assets/images/menu-collapsed.png" alt="menu-icon" />
+      <img v-else src="@/assets/images/close.png"/>
+    </button>
   </nav>
-  <router-view/>
+  <div v-if="menuIsOpen" class="mobile-menu-open">
+    <ul>
+      <li><router-link to="/" @click="toggleMenu">Home</router-link></li>
+      <li><router-link to="/about" @click="toggleMenu">About</router-link></li>
+      <li>
+        <router-link to="/contact" @click="toggleMenu">Contact</router-link>
+      </li>
+    </ul>
+  </div>
+  <router-view />
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
 #app {
- /*  background-image: url(@/assets/images/background.jpg);
+  /*  background-image: url(@/assets/images/background.jpg);
   background-size: cover; */
   min-height: 100vh;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+body {
+  margin: 0;
 }
 
 nav {
@@ -30,14 +62,32 @@ nav {
   padding: 30px;
 }
 
-nav a {
+nav a,
+li a {
   color: black;
   margin: 0 1rem;
   text-decoration: none;
+  text-transform: uppercase;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  list-style-type: none;
+  padding-top: 2rem;
 }
 
 nav a.router-link-exact-active {
   font-weight: bold;
+}
+
+button {
+  background: transparent;
+  border: none !important;
+  font-size: 0;
 }
 
 .navigation-menu {
@@ -46,5 +96,21 @@ nav a.router-link-exact-active {
   @media screen and (max-width: 768px) {
     display: none;
   }
+}
+
+.mobile-menu,
+.mobile-menu-open {
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+}
+
+.mobile-menu-open {
+  background: white;
+  width: 100vw;
+  height: auto;
+  position: absolute;
+  top: 6rem;
+  bottom: 0;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
-import ContactView from '../views/AboutView.vue'
+import ContactView from '../views/ContactView.vue'
 
 const routes = [
   {
